e640255a96e7a818279bd9f59870bbe6e4053a2d
Will Cai <84372812+wicai24@users.noreply.github.com>
2025-04-08 00:50:02 -0700

    Update run_all.sh

--- classifier/run_all.sh
+++ classifier/run_all.sh
@@ -8,19 +8,19 @@ OUTPUT_DIR="./output"
 mkdir -p $OUTPUT_DIR
 
 MODELS=(
-    # "Llama3.1-70B-it"
-    # "Llama3.1-70B-it-FP8"
-    # "Llama3.1-70B-it-INT8"
-    # "Gemma2-9b-it"
-    # "Gemma2-9b-it-FP8"
-    # "Gemma2-9b-it-INT8"
-    # "Qwen2.5-7b-it"
+    "Llama3.1-70B-it"
+    "Llama3.1-70B-it-FP8"
+    "Llama3.1-70B-it-INT8"
+    "Gemma2-9b-it"
+    "Gemma2-9b-it-FP8"
+    "Gemma2-9b-it-INT8"
+    "Qwen2.5-7b-it"
     "Qwen2-72B-it"
-    # "Qwen2-72B-it-FP8"
-    # "Qwen2-72B-it-INT8"
-    # "Mistral-7b-v3-it"
-    # "Mistral-7b-v3-it-FP8"
-    # "Mistral-7b-v3-it-INT8"
+    "Qwen2-72B-it-FP8"
+    "Qwen2-72B-it-INT8"
+    "Mistral-7b-v3-it"
+    "Mistral-7b-v3-it-FP8"
+    "Mistral-7b-v3-it-INT8"
 )
 
 for MODEL in "${MODELS[@]}"; do
